3c5abd59dd912f7a0b2ac9617440096c47763327
evelinavid <evelina198@gmail.com>
2023-01-24 14:33:31 +0200

    task: created CarCollection abstract

--- src/helpers/cars-collection.ts
+++ src/helpers/cars-collection.ts
@@ -1,9 +1,21 @@
+import type Car from "../types/car.js";
+import type Model from "../types/model.js";
+import type Brand from "../types/brand.js";
 type CarsCollectionProps = {
-
+car:Car[],
+brand: Brand[],
+model:Model[],
 };
 
 class CarsCollection {
-
+    private car: Car[];
+    private brand: Brand[];
+    private model: Model[];
+constructor({car, brand, model}:CarsCollectionProps){
+    this.car = JSON.parse(JSON.stringify(car));
+    this.brand = JSON.parse(JSON.stringify(brand));
+    this.model = JSON.parse(JSON.stringify(model));
+}
 }
 
 export default CarsCollection;
